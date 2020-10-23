d4da22333ef6f0b674c859e975e1a5970a7a0273
Henrik Bartholdsson <henrik.bartholdsson@hotmail.com>
2020-10-23 19:00:38 +0200

    Removed WebShop.Core project.

--- WebShop.API/Controllers/v1/OrdersController.cs
+++ WebShop.API/Controllers/v1/OrdersController.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebShop.Core.Service;
 using WebShop.Data.Models;
+using WebShop.Data.Models.Dto;
 using WebShop.Data.Repository.Contract;
 
 namespace WebShop.API.Controllers.v1
@@ -19,29 +23,48 @@ namespace WebShop.API.Controllers.v1
         {
             _unitOfWork = unitOfWork;
         }
+
         [HttpGet]
-        public IEnumerable<ORDER> GetOrder(int id)
+        public IActionResult GetOrder(int id)
         {
+            if (id == 0)
+                return BadRequest("");
+
             var orders = _unitOfWork.Order.GetOrderByOrderId(id);
 
+            if (orders == null)
+                return NotFound();
             // Use UnitOfWork
 
-            return orders;
+            return Ok(orders);
         }
 
         [HttpPost]
-        public void CreateOrders()
+        public void CreateOrder([FromBody]OrderInputDto input)
         {
-            var recAA = new ORDERRECORD { ItemName = "Hårddisk", ItemId = 1, Price= 123.3, ProductId = 123 };
-            var recAB = new ORDERRECORD { ItemName = "SSD", ItemId = 2, Price = 244.9, ProductId = 312 };
-            var orderA = new ORDER();
-            orderA.OrderRecords = new List<ORDERRECORD>();
-            orderA.OrderRecords.Add(recAA);
-            orderA.OrderRecords.Add(recAB);
-            orderA.UserId = "abc";
-            orderA.OrderInfo = "En order på lite saker";
-            _unitOfWork.Order.Add(orderA);
-            
+            var service = new OrderService();
+            var order = service.InputOrderToDomainModel(input);
+
+
+            _unitOfWork.Order.Add(order);
+
         }
+
+        // return Request.CreateResponse(HttpStatusCode.InternalServerError);
+
+        //[HttpPost]
+        //public void CreateOrders()
+        //{
+        //    var recAA = new ORDERRECORD { ItemName = "Hårddisk", ItemId = 1, Price= 123.3, ProductId = 123 };
+        //    var recAB = new ORDERRECORD { ItemName = "SSD", ItemId = 2, Price = 244.9, ProductId = 312 };
+        //    var orderA = new ORDER();
+        //    orderA.OrderRecords = new List<ORDERRECORD>();
+        //    orderA.OrderRecords.Add(recAA);
+        //    orderA.OrderRecords.Add(recAB);
+        //    orderA.UserId = "abc";
+        //    orderA.OrderInfo = "En order på lite saker";
+        //    _unitOfWork.Order.Add(orderA);
+
+        //}
     }
 }
